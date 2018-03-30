2084c2e0efbced74365e875094a416e4f75f7fe7
Renato Gomes <renatoo.agomes@gmail.com>
2018-03-30 19:09:21 -0300

    Removendo 'Auth::routes()' e registrando rotas de login manualmente, para nao haver rotas de register. Removendo UserController já que nao vamos precisar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>InfyOm Generator</title>
+    <title>Instituto Shanti - Área administrativa</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <!-- Bootstrap 3.3.7 -->
@@ -37,7 +37,7 @@
 
             <!-- Logo -->
             <a href="#" class="logo">
-                <b>InfyOm</b>
+                <b>Shanti</b>
             </a>
 
             <!-- Header Navbar -->
