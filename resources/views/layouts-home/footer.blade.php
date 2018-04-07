7032a9f338012c6ac82114e98de6b1d7ca37b3c8
Leandro Acquati <leandroacquati@gmail.com>
2018-04-07 01:09:24 -0300

    nav e footer finalizado, pendente alguns ajustes

--- resources/views/layouts-home/footer.blade.php
+++ resources/views/layouts-home/footer.blade.php
@@ -1,10 +1,42 @@
-<footer class="footer">
+<footer class="footer footer-pc">
   <h6>INSTITUTO SHANTI</h6>
-  <p><i class="fa fa-map-marker" aria-hidden="true"></i> Rua Maria José, 12-17 | Bauru - SP</p>
-  <p><i class="fa fa-phone" aria-hidden="true"></i> (14) 98832-5849   <i class="fa fa-envelope" aria-hidden="true"></i> contato@institutoshanti.com</p>
+  <a href="" target="_blank">
+    <i class="fa fa-map-marker" aria-hidden="true"></i>Rua Maria José, 12-17 | Bauru - SP
+  </a>
+  <a href="" target="_blank">
+    <i class="fa fa-phone" aria-hidden="true"></i>(14) 98832-5849
+  </a>
+  <a href="" target="_blank">
+    <i class="fa fa-envelope" aria-hidden="true"></i>contato@institutoshanti.com<br>
+  </a>
+  <a href="https://www.instagram.com/explore/locations/1014091440/instituto-shanti/?hl=pt-br" target="_blank">
+    <i class="fa fa-instagram" aria-hidden="true"></i>@shanticienciacorpomente
+  </a>
+  <a href="https://www.facebook.com/Shanticienciacorpomente" target="_blank">
+    <i class="fa fa-facebook-square" aria-hidden="true"></i>/Shanticienciacorpomente
+  </a>
 </footer>
 
-<ul class="nav justify-content-center">
+<footer class="footer footer-mobile">
+  <h6>INSTITUTO SHANTI</h6>
+  <a href="" target="_blank">
+    <i class="fa fa-map-marker" aria-hidden="true"></i>Rua Maria José, 12-17 | Bauru - SP
+  </a>
+  <a href="" target="_blank">
+    <i class="fa fa-phone" aria-hidden="true"></i>(14) 98832-5849
+  </a>
+  <a href="" target="_blank">
+    <i class="fa fa-envelope" aria-hidden="true"></i>contato@institutoshanti.com<br>
+  </a>
+  <a href="https://www.instagram.com/explore/locations/1014091440/instituto-shanti/?hl=pt-br" target="_blank">
+    <i class="fa fa-instagram" aria-hidden="true"></i>@Shanticienciacorpomente
+  </a>
+  <a href="https://www.facebook.com/Shanticienciacorpomente" target="_blank">
+    <i class="fa fa-facebook-square" aria-hidden="true"></i>/Shanticienciacorpomente
+  </a>
+</footer>
+
+<ul class="nav justify-content-center nav-pc">
   <li class="nav-item active">
     <a class="nav-link" href="#">O ESPAÇO</a>
   </li>
@@ -26,6 +58,28 @@
   <li class="nav-item">
     <a class="nav-link" href="#">CONTATO</a>
   </li>
-  <a href="" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-  <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+</ul>
+
+<ul class="nav flex-column justify-content-center nav-mobile">
+  <li class="nav-item active">
+    <a class="nav-link" href="#">O ESPAÇO</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">QUEM SOMOS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">SERVIÇOS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">PROGRAMAÇÃO</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">PORTIFÓLIO</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">ARTIGOS</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">CONTATO</a>
+  </li>
 </ul>
--- resources/views/layouts-home/nav.blade.php
+++ resources/views/layouts-home/nav.blade.php
@@ -12,13 +12,17 @@
   </div>
 </header>
 
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-pc">
   <div class="container">
     <a class="navbar-brand" href="#">
       <img src="images/logo-shanti.png">
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+      <span class="navbar-toggler-icon">
+        <div class="icon-bar"></div>
+        <div class="icon-bar"></div>
+        <div class="icon-bar"></div>
+      </span>
     </button>
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
       <ul class="navbar-nav">
@@ -47,3 +51,45 @@
     </div>
   </div>
 </nav>
+{{-- 
+<nav class="navbar navbar-expand-lg fixed-top navbar-mobile">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="images/logo-shanti.png">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+        <div class="icon-bar"></div>
+        <div class="icon-bar"></div>
+        <div class="icon-bar"></div>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">O ESPAÇO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">QUEM SOMOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">SERVIÇOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PROGRAMAÇÃO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">PORTIFÓLIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ARTIGOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CONTATO</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="navbar-space"></div> --}}
