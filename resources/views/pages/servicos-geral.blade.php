5b47041decc116ed67484e1f6189f355af0050a6
Leandro Acquati <leandroacquati@gmail.com>
2018-05-08 23:15:19 -0300

    servicos-geral

---
+++ resources/views/pages/servicos-geral.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts-home.master')
+
+@section('content')
+
+<div class="container servicos-geral">
+    <h4>SERVIÇOS ____</h4>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <a href="">
+                <h5>NATUROLOGIA</h5>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Ayurveda</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Medicina Tradicional Chinesa</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Medicinas Tradicionais Indígenas</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Aromaterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Auriculoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Cristalterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Cromopuntura</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Fitoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Florais de Bach</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Geoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Hidroterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Iridologia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Massoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Reflexoterapia</li>
+            </a>
+        </div>
+        <div class="col-12 col-lg-6">
+            <a href="">
+                <h5>YOGA</h5>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Grupos</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Personal e Yogaterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Yoga para gestantes</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Yoga para crianças</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Yoga para pais e filhos</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Yoga Hormonal</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Yoga para casais</li>
+            </a>
+        </div>
+        <div class="col-12 col-lg-6">
+            <a href="">
+                <h5>TERAPIAS</h5>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Astropsicologia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Massagem Ayurvédica</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Psicologia Transpessoal</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Regressão</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Orientação Familiar</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Orientação à pais</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Tarot Terapêutico</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Thetahealing</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Essência de Cristais de Oz</li>
+            </a>
+        </div>
+        <div class="col-12 col-lg-6">
+            <a href="">
+                <h5>CURSOS, EVENTOS E OUTROS</h5>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Astropsicologia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>EFT - Técnica de Libertação Emocional</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Reflexologia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Aromaterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Massoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Cristalterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Florais de Bach</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Fitoterapia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Iridologia</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Meditação</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Mantras</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Curso de Virtudes para Crianças</li>
+                <li><i class="fa fa-circle" aria-hidden="true"></i>Dragon Dreaming – Design de Projetos</li>
+            </a>
+        </div>                
+    </div>
+</div>
+
+@endsection
