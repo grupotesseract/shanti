24a07bab7cebd0eba149b9b0cb324c80da8e953d
Leandro Acquati <leandroacquati@gmail.com>
2018-06-03 12:14:12 -0300

    programacao cursos interno

--- resources/views/pages/programacao-cards.blade.php
+++ resources/views/pages/programacao-cards.blade.php
@@ -1,7 +1,9 @@
 <div class="row justify-content-center">
     <div class="col-12 col-lg-5">
-        <p class="caption">27/02 | Terça-Feira</p>            
-        <img src="images/500x300.png">
+        <a href="programacao-cursos-interno">
+            <p class="caption">27/02 | Terça-Feira</p>            
+            <img src="images/500x300.png">
+        </a>
     </div>
     <div class="col-12 col-lg-5 align-self-end">            
         <h5>Nome do Evento</h5>
@@ -10,10 +12,12 @@
             <i class="fab fa-facebook-square fa-2x"></i>
         </a>
     </div>
-    
+
     <div class="col-12 col-lg-5">
-        <p class="caption">27/02 | Terça-Feira</p>            
-        <img src="images/500x300.png">
+        <a href="programacao-cursos-interno">
+            <p class="caption">27/02 | Terça-Feira</p>            
+            <img src="images/500x300.png">
+        </a>
     </div>
     <div class="col-12 col-lg-5 align-self-end">            
         <h5>Nome do Evento</h5>
@@ -22,10 +26,12 @@
             <i class="fab fa-facebook-square fa-2x"></i>
         </a>
     </div>
-    
+
     <div class="col-12 col-lg-5">
-        <p class="caption">27/02 | Terça-Feira</p>            
-        <img src="images/500x300.png">
+        <a href="programacao-cursos-interno">
+            <p class="caption">27/02 | Terça-Feira</p>            
+            <img src="images/500x300.png">
+        </a>
     </div>
     <div class="col-12 col-lg-5 align-self-end">            
         <h5>Nome do Evento</h5>
@@ -34,10 +40,12 @@
             <i class="fab fa-facebook-square fa-2x"></i>
         </a>
     </div>
-    
+
     <div class="col-12 col-lg-5">
-        <p class="caption">27/02 | Terça-Feira</p>            
-        <img src="images/500x300.png">
+        <a href="programacao-cursos-interno">
+            <p class="caption">27/02 | Terça-Feira</p>            
+            <img src="images/500x300.png">
+        </a>
     </div>
     <div class="col-12 col-lg-5 align-self-end">            
         <h5>Nome do Evento</h5>
