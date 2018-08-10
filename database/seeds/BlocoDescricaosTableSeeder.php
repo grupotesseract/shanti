a17cfc10d7b893e8ce0527e11488b85b0d0eda1f
Renato Gomes <renatoo.agomes@gmail.com>
2018-08-10 19:42:19 -0300

    Fix seeds faziam ignorar as fk's , problema do postgres ou do iseed??

--- database/seeds/BlocoDescricaosTableSeeder.php
+++ database/seeds/BlocoDescricaosTableSeeder.php
@@ -16,58 +16,36 @@ class BlocoDescricaosTableSeeder extends Seeder
 
         \DB::table('bloco_descricaos')->delete();
         
-        \DB::table('bloco_descricaos')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 1,
                 'ordem' => 1,
                 'json_conteudo' => '{"texto":"Quando o finito entra em contato com o infinito, isto \\u00e9 chamado Yoga","autor":"Shrii Shrii Anandamurti"}',
-                'created_at' => '2018-08-08 22:48:49',
-                'updated_at' => '2018-08-08 22:48:49',
                 'profissional_id' => 1,
-            ),
-            1 => 
-            array (
-                'id' => 2,
+            ));
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 4,
                 'ordem' => 3,
                 'json_conteudo' => '{"titulo":"Aulas - Modalidades","items":["Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais",null,null,null,null,null]}',
-                'created_at' => '2018-08-08 22:49:27',
-                'updated_at' => '2018-08-08 22:49:42',
                 'profissional_id' => 1,
-            ),
-            2 => 
-            array (
-                'id' => 3,
+            ));
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 3,
                 'ordem' => 4,
                 'json_conteudo' => '{"texto":"Venha Fazer uma aula experimental","url":"https:\\/\\/www.facebook.com\\/Shanticienciacorpomente\\/"}',
-                'created_at' => '2018-08-08 22:51:48',
-                'updated_at' => '2018-08-08 22:51:48',
                 'profissional_id' => 1,
-            ),
-            3 => 
-            array (
-                'id' => 4,
+            ));
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 5,
                 'ordem' => 5,
                 'json_conteudo' => '{"src":"\\/\\/res.cloudinary.com\\/tesseract\\/image\\/upload\\/c_scale,g_center,h_450,w_800\\/shanti_profissional_1533779536.jpg"}',
-                'created_at' => '2018-08-08 22:52:20',
-                'updated_at' => '2018-08-08 22:52:20',
                 'profissional_id' => 1,
-            ),
-            4 => 
-            array (
-                'id' => 5,
+            ));
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 2,
                 'ordem' => 8,
                 'json_conteudo' => '{"texto":"<p>Idealizador e s&oacute;cio propriet&aacute;rio do Instituto Shanti de Ci&ecirc;ncia Corpo Mente. Representante do Instituto Roerich da Cultura e da Paz do Brasil, membro da Comiss&atilde;o Municipal da Cultura de Paz e fundador do f&oacute;rum Caminhos para a Sustentabilidade. Atua em consult&oacute;rio naturol&oacute;gico e ministra cursos e palestras na &aacute;rea desde 2007.<\\/p>"}',
-                'created_at' => '2018-08-08 23:02:27',
-                'updated_at' => '2018-08-08 23:15:48',
                 'profissional_id' => 2,
-            ),
-        ));
+            ));
         
         
     }
--- database/seeds/FotosTableSeeder.php
+++ database/seeds/FotosTableSeeder.php
@@ -16,74 +16,46 @@ class FotosTableSeeder extends Seeder
 
         \DB::table('fotos')->delete();
         
-        \DB::table('fotos')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+        \App\Models\Foto::create(array (
                 'cloudinary_id' => 'shanti_profissional_1533779218',
                 'image_name' => '1533779217',
                 'image_path' => '/var/www/public/uploads/',
                 'image_extension' => 'jpg',
                 'owner_id' => 1,
                 'owner_type' => 'App\\Models\\Profissional',
-                'created_at' => '2018-08-08 22:46:57',
-                'updated_at' => '2018-08-08 22:47:01',
-                'deleted_at' => NULL,
-            ),
-            1 => 
-            array (
-                'id' => 2,
+            ));
+        \App\Models\Foto::create(array (
                 'cloudinary_id' => 'shanti_profissional_1533779536',
                 'image_name' => '1533779536',
                 'image_path' => '/var/www/public/uploads/',
                 'image_extension' => 'png',
                 'owner_id' => NULL,
                 'owner_type' => NULL,
-                'created_at' => '2018-08-08 22:52:16',
-                'updated_at' => '2018-08-08 22:52:20',
-                'deleted_at' => NULL,
-            ),
-            2 => 
-            array (
-                'id' => 3,
+            ));
+        \App\Models\Foto::create(array (
                 'cloudinary_id' => 'shanti_profissional_1533780130',
                 'image_name' => '1533780130',
                 'image_path' => '/var/www/public/uploads/',
                 'image_extension' => 'jpeg',
                 'owner_id' => 2,
                 'owner_type' => 'App\\Models\\Profissional',
-                'created_at' => '2018-08-08 23:02:10',
-                'updated_at' => '2018-08-08 23:02:14',
-                'deleted_at' => NULL,
-            ),
-            3 => 
-            array (
-                'id' => 4,
+            ));
+        \App\Models\Foto::create(array (
                 'cloudinary_id' => 'shanti_profissional_1533780510',
                 'image_name' => '1533780510',
                 'image_path' => '/var/www/public/uploads/',
                 'image_extension' => 'jpg',
                 'owner_id' => 3,
                 'owner_type' => 'App\\Models\\Profissional',
-                'created_at' => '2018-08-08 23:08:30',
-                'updated_at' => '2018-08-08 23:09:14',
-                'deleted_at' => '2018-08-08 23:09:14',
-            ),
-            4 => 
-            array (
-                'id' => 5,
+            ));
+        \App\Models\Foto::create(array (
                 'cloudinary_id' => 'shanti_profissional_1533780554',
                 'image_name' => '1533780554',
                 'image_path' => '/var/www/public/uploads/',
                 'image_extension' => 'jpg',
                 'owner_id' => 3,
                 'owner_type' => 'App\\Models\\Profissional',
-                'created_at' => '2018-08-08 23:09:14',
-                'updated_at' => '2018-08-08 23:09:17',
-                'deleted_at' => NULL,
-            ),
-        ));
-        
+            ));
         
     }
 }
