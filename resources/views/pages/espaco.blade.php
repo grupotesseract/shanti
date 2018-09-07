6abacc7ffbdb253087a885eadce25f40abaf5de1
Renato Gomes <renatoo.agomes@gmail.com>
2018-09-07 20:50:07 -0300

    Trocando imagens da pagina 'O Espaço'. Usando Agent para nao quebrar o JS do bootstrap, renderizando conteudos diferentes direto na view

--- resources/views/pages/espaco.blade.php
+++ resources/views/pages/espaco.blade.php
@@ -9,60 +9,124 @@
 </style>
 
 <div class="container">
-	<div class="espaco-mobile">
-		<div id="carouselExampleIndicators" class="carousel slide carousel-espaco" data-ride="carousel">
-		  <ol class="carousel-indicators">
-		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-		  </ol>
-		  <div class="carousel-inner">
-		    <div class="carousel-item active">
-		      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a948%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a948%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9296875%22%20y%3D%22217.7%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="First slide">
-		    </div>
-		    <div class="carousel-item">
-		      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a94b%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a94b%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22217.7%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Second slide">
-		    </div>
-		    <div class="carousel-item">
-		      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a94e%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a94e%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277.0078125%22%20y%3D%22217.7%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Third slide">
-		    </div>
-		  </div>
-		</div>
-	</div>
+    @if (\Agent::isMobile())
+        <div class="espaco-mobile">
+            <div id="carouselExampleIndicators" class="carousel slide carousel-espaco" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362064/shanti/_DSC1122.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362063/shanti/_DSC1181.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362063/shanti/_DSC1139.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362062/shanti/39296912_1792701990798438_3132007516519530496_n.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39265655_1792700344131936_7233709693745496064_n.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39211756_1792700267465277_1230123142320488448_n.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39253540_1792701227465181_7228503295734579200_n.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39252900_1792701087465195_1242835678580965376_n.jpg" alt="Third slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39208334_1792701450798492_1808732078351581184_n.jpg" alt="Third slide">
+                    </div>
+                </div>
+            </div>
+        <div class="row">
+            <div class="col col-espaco">
+                <h4>O ESPAÇO __</h4>
+                <p>Shanti, palavra sânscrita que significa PAZ, tornou-se uma síntese do objetivo deste Instituto, idealizado e fundado em fevereiro de 2015 pela professora de Yoga Gabriela Teixeira e pelo naturólogo João G. G. Maciel.</p>
+                <p>O Instituto Shanti oferece serviços voltados para a saúde integral, promoção da cultura de paz, do desenvolvimento sustentável e expansão da consciência.</p>
 
-	<div class="row">
-		<div class="col col-espaco">
-			<h4>O ESPAÇO __</h4>
-			<p>Shanti, palavra sânscrita que significa PAZ, tornou-se uma síntese do objetivo deste Instituto, idealizado e fundado em fevereiro de 2015 pela professora de Yoga Gabriela Teixeira e pelo naturólogo João G. G. Maciel.</p>
-			<p>O Instituto Shanti oferece serviços voltados para a saúde integral, promoção da cultura de paz, do desenvolvimento sustentável e expansão da consciência.</p>
+                <blockquote class="blockquote text-center">
+                    <p class="mb-1">“A parte mais doce do jogo da Consciência Infinita é que Ela está escondida em cada um e cada um está procurando por ela.”</p>
+                    <footer class="blockquote-footer">Sri Sri Anandamurti</footer>
+                </blockquote>
+            </div>
+        </div>
+            
+        </div>
 
-			<blockquote class="blockquote text-center">
-			  <p class="mb-1">“A parte mais doce do jogo da Consciência Infinita é que Ela está escondida em cada um e cada um está procurando por ela.”</p>
-			  <footer class="blockquote-footer">Sri Sri Anandamurti</footer>
-			</blockquote>
-		</div>
-		
-		<div class="col-8 espaco-pc">
-			<div id="carouselExampleIndicators" class="carousel slide carousel-espaco" data-ride="carousel">
-			  <ol class="carousel-indicators">
-			    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-			    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-			    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-			  </ol>
-			  <div class="carousel-inner">
-			    <div class="carousel-item active">
-			      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a948%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a948%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.9296875%22%20y%3D%22217.7%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="First slide">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a94b%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a94b%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22217.7%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Second slide">
-			    </div>
-			    <div class="carousel-item">
-			      <img class="d-block w-100" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1628280a94e%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1628280a94e%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277.0078125%22%20y%3D%22217.7%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Third slide">
-			    </div>
-			  </div>
-			</div>
-		</div>
-	</div>
+    @else
+        <div class="row">
+            <div class="col col-espaco">
+                <h4>O ESPAÇO __</h4>
+                <p>Shanti, palavra sânscrita que significa PAZ, tornou-se uma síntese do objetivo deste Instituto, idealizado e fundado em fevereiro de 2015 pela professora de Yoga Gabriela Teixeira e pelo naturólogo João G. G. Maciel.</p>
+                <p>O Instituto Shanti oferece serviços voltados para a saúde integral, promoção da cultura de paz, do desenvolvimento sustentável e expansão da consciência.</p>
+
+                <blockquote class="blockquote text-center">
+                    <p class="mb-1">“A parte mais doce do jogo da Consciência Infinita é que Ela está escondida em cada um e cada um está procurando por ela.”</p>
+                    <footer class="blockquote-footer">Sri Sri Anandamurti</footer>
+                </blockquote>
+            </div>
+
+            <div class="col-8 espaco-pc">
+                <div id="carouselExampleIndicators" class="carousel slide carousel-espaco" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362064/shanti/_DSC1122.jpg" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362063/shanti/_DSC1181.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362063/shanti/_DSC1139.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362062/shanti/39296912_1792701990798438_3132007516519530496_n.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39265655_1792700344131936_7233709693745496064_n.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39211756_1792700267465277_1230123142320488448_n.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39253540_1792701227465181_7228503295734579200_n.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39252900_1792701087465195_1242835678580965376_n.jpg" alt="Third slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://res.cloudinary.com/tesseract/image/upload/v1536362061/shanti/39208334_1792701450798492_1808732078351581184_n.jpg" alt="Third slide">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
 
 @endsection
