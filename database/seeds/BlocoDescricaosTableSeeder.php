baab34c5a7c50b994a6f07c80cfe5df574e588ad
Renato Gomes <renatoo.agomes@gmail.com>
2018-09-09 14:50:30 -0300

    merged pos iseed

--- database/seeds/BlocoDescricaosTableSeeder.php
+++ database/seeds/BlocoDescricaosTableSeeder.php
@@ -12,63 +12,57 @@ class BlocoDescricaosTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
         \DB::table('bloco_descricaos')->delete();
         
-        \DB::table('bloco_descricaos')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 1,
                 'ordem' => 1,
-                'json_conteudo' => '{"texto":"Quando o finito entra em contato com o infinito, isto \\u00e9 chamado Yoga","autor":"Shrii Shrii Anandamurti"}',
+                'json_conteudo' => json_decode( '{"texto":"Quando o finito entra em contato com o infinito, isto \\u00e9 chamado Yoga","autor":"Shrii Shrii Anandamurti"}'),
                 'created_at' => '2018-08-14 21:11:55',
                 'updated_at' => '2018-08-14 21:11:55',
-                'profissional_id' => 1,
-            ),
-            1 => 
-            array (
-                'id' => 2,
+                'owner_id' => 1,
+                'owner_type' => 'App\\Models\\Profissional',
+            ));
+
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 4,
                 'ordem' => 3,
-                'json_conteudo' => '{"titulo":"Aulas - Modalidades","items":["Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais",null,null,null,null,null]}',
+                'json_conteudo' => json_decode( '{"titulo":"Aulas - Modalidades","items":["Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais","Aulas em grupo e individuais",null,null,null,null,null]}'),
                 'created_at' => '2018-08-14 21:11:55',
                 'updated_at' => '2018-08-14 21:11:55',
-                'profissional_id' => 1,
-            ),
-            2 => 
-            array (
-                'id' => 4,
+                'owner_id' => 1,
+                'owner_type' => 'App\\Models\\Profissional',
+            ));
+
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 5,
                 'ordem' => 5,
-                'json_conteudo' => '{"src":"\\/\\/res.cloudinary.com\\/tesseract\\/image\\/upload\\/c_scale,g_center,h_450,w_800\\/shanti_profissional_1533779536.jpg"}',
+                'json_conteudo' => json_decode( '{"src":"\\/\\/res.cloudinary.com\\/tesseract\\/image\\/upload\\/c_scale,g_center,h_450,w_800\\/shanti_profissional_1533779536.jpg"}'),
                 'created_at' => '2018-08-14 21:11:55',
                 'updated_at' => '2018-08-14 21:11:55',
-                'profissional_id' => 1,
-            ),
-            3 => 
-            array (
-                'id' => 5,
+                'owner_id' => 1,
+                'owner_type' => 'App\\Models\\Profissional',
+            ));
+
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 2,
                 'ordem' => 8,
-                'json_conteudo' => '{"texto":"<p>Idealizador e s&oacute;cio propriet&aacute;rio do Instituto Shanti de Ci&ecirc;ncia Corpo Mente. Representante do Instituto Roerich da Cultura e da Paz do Brasil, membro da Comiss&atilde;o Municipal da Cultura de Paz e fundador do f&oacute;rum Caminhos para a Sustentabilidade. Atua em consult&oacute;rio naturol&oacute;gico e ministra cursos e palestras na &aacute;rea desde 2007.<\\/p>"}',
+                'json_conteudo' => json_decode( '{"texto":"<p>Idealizador e s&oacute;cio propriet&aacute;rio do Instituto Shanti de Ci&ecirc;ncia Corpo Mente. Representante do Instituto Roerich da Cultura e da Paz do Brasil, membro da Comiss&atilde;o Municipal da Cultura de Paz e fundador do f&oacute;rum Caminhos para a Sustentabilidade. Atua em consult&oacute;rio naturol&oacute;gico e ministra cursos e palestras na &aacute;rea desde 2007.<\\/p>"}'),
                 'created_at' => '2018-08-14 21:11:55',
                 'updated_at' => '2018-08-14 21:11:55',
-                'profissional_id' => 2,
-            ),
-            4 => 
-            array (
-                'id' => 6,
+                'owner_id' => 2,
+                'owner_type' => 'App\\Models\\Profissional',
+            ));
+
+        \App\Models\BlocoDescricao::create(array (
                 'tipo' => 3,
                 'ordem' => 9,
-                'json_conteudo' => '{"texto":"Conhe\\u00e7a nosso FB","url":"https:\\/\\/www.facebook.com\\/Shanticienciacorpomente\\/"}',
+                'json_conteudo' => json_decode( '{"texto":"Conhe\\u00e7a nosso FB","url":"https:\\/\\/www.facebook.com\\/Shanticienciacorpomente\\/"}'),
                 'created_at' => '2018-08-14 21:20:51',
                 'updated_at' => '2018-08-14 21:20:51',
-                'profissional_id' => 1,
-            ),
-        ));
-        
+                'owner_id' => 1,
+                'owner_type' => 'App\\Models\\Profissional',
+            ));
         
     }
 }
--- database/seeds/GrupoServicosTableSeeder.php
+++ database/seeds/GrupoServicosTableSeeder.php
@@ -12,155 +12,151 @@ class GrupoServicosTableSeeder extends Seeder
      */
     public function run()
     {
-        
-
         \DB::table('grupo_servicos')->delete();
-        
-        \DB::table('grupo_servicos')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'titulo' => 'YOGA',
-                'descricao_grupo' => '<p>&nbsp; &nbsp; &nbsp;Yoga &eacute; um caminho que permite ao ser humano expressar e recuperar suas potencialidades, &eacute; uma ferramenta de promo&ccedil;&atilde;o da sa&uacute;de integral, uma vez que considera o corpo-mente humano em toda sua complexidade e em todos os seus aspectos: f&iacute;sicos, mentais e espirituais. A pr&aacute;tica do Yoga permite ao indiv&iacute;duo entrar em contato com o que h&aacute; de mais profundo em si, trazendo-lhe maior autoconhecimento, autocontrole e bem estar, al&eacute;m de promover o equil&iacute;brio psicofisiol&oacute;gico e possibilitar a autonomia do praticante, apresentando-se tamb&eacute;m como uma excelente ferramenta terap&ecirc;utica.</p>',
-                'descricao_servicos' => '<h2>GRUPOS</h2>
 
-<p>As aulas s&atilde;o compostas por diversos elementos que visam o autoconhecimento, a sa&uacute;de f&iacute;sica e mental, dentre eles destacam-se:</p>
+        \App\Models\GrupoServico::create(array (
 
-<p>- &Aacute;sanas, posturas psicof&iacute;sicas que visam o equil&iacute;brio org&acirc;nico e fisiol&oacute;gico do corpo, interferem na respira&ccedil;&atilde;o, no suprimento sangu&iacute;neo, no alongamento, fortalecimento e relaxamento muscular, atuando diretamente no sistema nervoso aut&ocirc;nomo e nas gl&acirc;ndulas end&oacute;crinas;</p>
+            'titulo' => 'YOGA',
+            'descricao_grupo' => '<p>&nbsp; &nbsp; &nbsp;Yoga &eacute; um caminho que permite ao ser humano expressar e recuperar suas potencialidades, &eacute; uma ferramenta de promo&ccedil;&atilde;o da sa&uacute;de integral, uma vez que considera o corpo-mente humano em toda sua complexidade e em todos os seus aspectos: f&iacute;sicos, mentais e espirituais. A pr&aacute;tica do Yoga permite ao indiv&iacute;duo entrar em contato com o que h&aacute; de mais profundo em si, trazendo-lhe maior autoconhecimento, autocontrole e bem estar, al&eacute;m de promover o equil&iacute;brio psicofisiol&oacute;gico e possibilitar a autonomia do praticante, apresentando-se tamb&eacute;m como uma excelente ferramenta terap&ecirc;utica.</p>',
+            'descricao_servicos' => '<h2>GRUPOS</h2>
 
-<p>- Pr&aacute;ticas respirat&oacute;rias, que atuam diretamente sobre o fluxo ps&iacute;quico energ&eacute;tico do corpo (o Prana), purificando as correntes sutis (Nadiis), facilitando a concentra&ccedil;&atilde;o, promovendo a interioza&ccedil;&atilde;o da aten&ccedil;&atilde;o e possibilitando que o praticante atinja estados profundos de medita&ccedil;&atilde;o, uma vez que a atividade da respira&ccedil;&atilde;o e a atividade da mente se interferem mutuamente;</p>
+            <p>As aulas s&atilde;o compostas por diversos elementos que visam o autoconhecimento, a sa&uacute;de f&iacute;sica e mental, dentre eles destacam-se:</p>
 
-<p>- Medita&ccedil;&atilde;o: A medita&ccedil;&atilde;o constitui-se como um processo cont&iacute;nuo que envolve diferentes etapas, seu princ&iacute;pio &eacute; o desenvolvimento da interioriza&ccedil;&atilde;o, controle e direcionamento da energia mental atrav&eacute;s de t&eacute;cnicas espec&iacute;ficas que envolvem a focaliza&ccedil;&atilde;o da aten&ccedil;&atilde;o.</p>
+            <p>- &Aacute;sanas, posturas psicof&iacute;sicas que visam o equil&iacute;brio org&acirc;nico e fisiol&oacute;gico do corpo, interferem na respira&ccedil;&atilde;o, no suprimento sangu&iacute;neo, no alongamento, fortalecimento e relaxamento muscular, atuando diretamente no sistema nervoso aut&ocirc;nomo e nas gl&acirc;ndulas end&oacute;crinas;</p>
 
-<p>&nbsp;</p>
+            <p>- Pr&aacute;ticas respirat&oacute;rias, que atuam diretamente sobre o fluxo ps&iacute;quico energ&eacute;tico do corpo (o Prana), purificando as correntes sutis (Nadiis), facilitando a concentra&ccedil;&atilde;o, promovendo a interioza&ccedil;&atilde;o da aten&ccedil;&atilde;o e possibilitando que o praticante atinja estados profundos de medita&ccedil;&atilde;o, uma vez que a atividade da respira&ccedil;&atilde;o e a atividade da mente se interferem mutuamente;</p>
 
-<h2>PERSONAL E YOGATERAPIA</h2>
+            <p>- Medita&ccedil;&atilde;o: A medita&ccedil;&atilde;o constitui-se como um processo cont&iacute;nuo que envolve diferentes etapas, seu princ&iacute;pio &eacute; o desenvolvimento da interioriza&ccedil;&atilde;o, controle e direcionamento da energia mental atrav&eacute;s de t&eacute;cnicas espec&iacute;ficas que envolvem a focaliza&ccedil;&atilde;o da aten&ccedil;&atilde;o.</p>
 
-<p>Utilizando os mesmos elementos das pr&aacute;ticas em grupo, atuam de forma mais direcionada &agrave;s necessidades de cada indiv&iacute;duo.</p>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <h2>PERSONAL E YOGATERAPIA</h2>
 
-<h2>YOGA PARA GESTANTES</h2>
+            <p>Utilizando os mesmos elementos das pr&aacute;ticas em grupo, atuam de forma mais direcionada &agrave;s necessidades de cada indiv&iacute;duo.</p>
 
-<p>O Yoga traz muitos benef&iacute;cios &agrave; gesta&ccedil;&atilde;o, promovendo bem estar e equil&iacute;brio neste momento de t&atilde;o intensas transforma&ccedil;&otilde;es. Al&eacute;m de fortalecer o v&iacute;nculo entre m&atilde;e e beb&ecirc; desde o in&iacute;cio, garante uma gravidez mais tranquila e prepara a m&atilde;e para o parto e p&oacute;s-parto.</p>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <h2>YOGA PARA GESTANTES</h2>
 
-<h2>YOGA PARA CRIAN&Ccedil;AS</h2>
+            <p>O Yoga traz muitos benef&iacute;cios &agrave; gesta&ccedil;&atilde;o, promovendo bem estar e equil&iacute;brio neste momento de t&atilde;o intensas transforma&ccedil;&otilde;es. Al&eacute;m de fortalecer o v&iacute;nculo entre m&atilde;e e beb&ecirc; desde o in&iacute;cio, garante uma gravidez mais tranquila e prepara a m&atilde;e para o parto e p&oacute;s-parto.</p>
 
-<p>A pr&aacute;tica do Yoga na inf&acirc;ncia tem o objetivo de promover o desenvolvimento psicof&iacute;sico harmonioso da crian&ccedil;a, cultivando o autoconhecimento, a empatia e a livre express&atilde;o de suas potencialidades, criatividade e espontaneidade.</p>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <h2>YOGA PARA CRIAN&Ccedil;AS</h2>
 
-<h2>YOGA PARA PAIS E FILHOS</h2>
+            <p>A pr&aacute;tica do Yoga na inf&acirc;ncia tem o objetivo de promover o desenvolvimento psicof&iacute;sico harmonioso da crian&ccedil;a, cultivando o autoconhecimento, a empatia e a livre express&atilde;o de suas potencialidades, criatividade e espontaneidade.</p>
 
-<p>Uma excelente oportunidade para juntos realizarem posturas de Yoga, exerc&iacute;cios respirat&oacute;rios, massagem, relaxamento, medita&ccedil;&atilde;o e desenvolverem valores humanos de forma l&uacute;dica. Sugest&atilde;o de faixa et&aacute;ria da crian&ccedil;a: a partir de 04 anos.</p>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <h2>YOGA PARA PAIS E FILHOS</h2>
 
-<h2>YOGA PARA TERCEIRA IDADE&nbsp;</h2>
+            <p>Uma excelente oportunidade para juntos realizarem posturas de Yoga, exerc&iacute;cios respirat&oacute;rios, massagem, relaxamento, medita&ccedil;&atilde;o e desenvolverem valores humanos de forma l&uacute;dica. Sugest&atilde;o de faixa et&aacute;ria da crian&ccedil;a: a partir de 04 anos.</p>
 
-<p>Praticar Yoga com frequ&ecirc;ncia traz muitos benef&iacute;cios para a sa&uacute;de f&iacute;sica e mental e pode ser uma &oacute;tima ferramenta tamb&eacute;m para viver a terceira idade de forma mais tranquila e saud&aacute;vel. De forma leve e consciente, a pr&aacute;tica fortalece os m&uacute;sculos, melhora a flexibilidade das articula&ccedil;&otilde;es e o equil&iacute;brio, ajudando a diminuir a incid&ecirc;ncia de quedas e fraturas. Trabalha e fortalece a coluna vertebral, melhorando a postura, diminuindo as dores e trazendo maior conforto f&iacute;sico. Al&eacute;m disso, o Yoga ajuda a diminuir a press&atilde;o arterial, melhora a respira&ccedil;&atilde;o, diminui a ansiedade, o estresse, a mem&oacute;ria e capacidade de concentra&ccedil;&atilde;o.</p>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <h2>YOGA PARA TERCEIRA IDADE&nbsp;</h2>
 
-<p>&nbsp;</p>
+            <p>Praticar Yoga com frequ&ecirc;ncia traz muitos benef&iacute;cios para a sa&uacute;de f&iacute;sica e mental e pode ser uma &oacute;tima ferramenta tamb&eacute;m para viver a terceira idade de forma mais tranquila e saud&aacute;vel. De forma leve e consciente, a pr&aacute;tica fortalece os m&uacute;sculos, melhora a flexibilidade das articula&ccedil;&otilde;es e o equil&iacute;brio, ajudando a diminuir a incid&ecirc;ncia de quedas e fraturas. Trabalha e fortalece a coluna vertebral, melhorando a postura, diminuindo as dores e trazendo maior conforto f&iacute;sico. Al&eacute;m disso, o Yoga ajuda a diminuir a press&atilde;o arterial, melhora a respira&ccedil;&atilde;o, diminui a ansiedade, o estresse, a mem&oacute;ria e capacidade de concentra&ccedil;&atilde;o.</p>
 
-<h2>&nbsp;</h2>',
-                'lista_servicos' => '"\\"Grupos, Personal e Yogaterapia, Yoga para gestantes, Yoga para crian\\u00e7as, Yoga para pais e filhos, Yoga Hormonal, Yoga para casais\\""',
-                'created_at' => '2018-08-14 21:11:55',
-                'updated_at' => '2018-09-01 11:01:52',
-                'deleted_at' => NULL,
-            ),
-            1 => 
-            array (
-                'id' => 3,
-                'titulo' => 'TERAPIAS',
-                'descricao_grupo' => '<p>...</p>',
-                'descricao_servicos' => '<ul>
-<li>ASTROPSICOLOGIA&nbsp;</li>
-</ul>
+            <p>&nbsp;</p>
 
-<p>&nbsp;</p>
+            <p>&nbsp;</p>
 
-<ul>
-<li>MASSAGEM AYURV&Eacute;DICA&nbsp;</li>
-</ul>
+            <h2>&nbsp;</h2>',
+'lista_servicos' => '"\\"Grupos, Personal e Yogaterapia, Yoga para gestantes, Yoga para crian\\u00e7as, Yoga para pais e filhos, Yoga Hormonal, Yoga para casais\\""',
+'created_at' => '2018-08-14 21:11:55',
+'updated_at' => '2018-09-01 11:01:52',
+'deleted_at' => NULL,
+            ));
 
-<p>&nbsp;</p>
+\App\Models\GrupoServico::create(array (
 
-<ul>
-<li>PSICOLOGIA TRANSPESSOAL: A Psicologia Transpessoal &eacute; a &uacute;ltima grande for&ccedil;a dentro da Psicologia. Nela s&atilde;o aplicadas t&eacute;cnicas terap&ecirc;uticas que estimulam e potencializam toda capacidade de equil&iacute;brio da mente e do esp&iacute;rito.</li>
-</ul>
+    'titulo' => 'TERAPIAS',
+    'descricao_grupo' => '<p>...</p>',
+    'descricao_servicos' => '<ul>
+    <li>ASTROPSICOLOGIA&nbsp;</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>PSICOLOGIA CORPORAL:</li>
-</ul>
+    <ul>
+    <li>MASSAGEM AYURV&Eacute;DICA&nbsp;</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>REGRESS&Atilde;O: Terapia Regressiva &agrave; Viv&ecirc;ncias Passadas. M&eacute;todo terap&ecirc;utico para libera&ccedil;&atilde;o de traumas conscientes ou inconscientes.</li>
-</ul>
+    <ul>
+    <li>PSICOLOGIA TRANSPESSOAL: A Psicologia Transpessoal &eacute; a &uacute;ltima grande for&ccedil;a dentro da Psicologia. Nela s&atilde;o aplicadas t&eacute;cnicas terap&ecirc;uticas que estimulam e potencializam toda capacidade de equil&iacute;brio da mente e do esp&iacute;rito.</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>ORIENTA&Ccedil;&Atilde;O FAMILIAR: Identifica&ccedil;&atilde;o dos padr&otilde;es presentes nas din&acirc;micas familiares para transforma&ccedil;&atilde;o e harmoniza&ccedil;&atilde;o do conv&iacute;vio.</li>
-</ul>
+    <ul>
+    <li>PSICOLOGIA CORPORAL:</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>ORIENTA&Ccedil;&Atilde;O &Agrave; PAIS:&nbsp; Orienta&ccedil;&atilde;o para que os pais possam entender melhor as fases do desenvolvimento dos filhos e agir de acordo com a necessidade de cada fase.</li>
-</ul>
+    <ul>
+    <li>REGRESS&Atilde;O: Terapia Regressiva &agrave; Viv&ecirc;ncias Passadas. M&eacute;todo terap&ecirc;utico para libera&ccedil;&atilde;o de traumas conscientes ou inconscientes.</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>THETAHEALING: &nbsp;Poderosa t&eacute;cnica de cura baseada em conceitos da f&iacute;sica qu&acirc;ntica, parte da ideia de que criamos nossa realidade a partir das nossas cren&ccedil;as ou mem&oacute;rias energ&eacute;ticas do nosso DNA.</li>
-</ul>
+    <ul>
+    <li>ORIENTA&Ccedil;&Atilde;O FAMILIAR: Identifica&ccedil;&atilde;o dos padr&otilde;es presentes nas din&acirc;micas familiares para transforma&ccedil;&atilde;o e harmoniza&ccedil;&atilde;o do conv&iacute;vio.</li>
+    </ul>
 
-<p>&nbsp;</p>
+    <p>&nbsp;</p>
 
-<ul>
-<li>ESS&Ecirc;NCIA DE CRISTAIS DE OZ: S&atilde;o ess&ecirc;ncias de cristais elaboradas com a finalidade de recarregar a energia vital do corpo f&iacute;sico e emocional, possibilitando seu pleno equil&iacute;brio e funcionamento</li>
-</ul>',
-                'lista_servicos' => '"\\"Astropsicologia, Massagem Ayurv\\u00e9dica, Psicologia Transpessoal, Regress\\u00e3o, Orienta\\u00e7\\u00e3o Familiar, Orienta\\u00e7\\u00e3o \\u00e1 pais, Tarot Terap\\u00eautico, Thetahealing, Ess\\u00eancia de Cristais de Oz\\""',
-                'created_at' => '2018-08-14 21:11:55',
-                'updated_at' => '2018-09-01 11:10:22',
-                'deleted_at' => NULL,
-            ),
-            2 => 
-            array (
-                'id' => 2,
-                'titulo' => 'NATUROLOGIA',
-            'descricao_grupo' => '<p>Com base nas Medicinas Tradicionais Indiana (Ayurveda), Chinesa e Ind&iacute;gena a Naturologia atua na recupera&ccedil;&atilde;o e promo&ccedil;&atilde;o da sa&uacute;de integral, tratando os sintomas e suas causas mais profundas de forma segura e eficiente por meio das terapias naturais:&nbsp;</p>
+    <ul>
+    <li>ORIENTA&Ccedil;&Atilde;O &Agrave; PAIS:&nbsp; Orienta&ccedil;&atilde;o para que os pais possam entender melhor as fases do desenvolvimento dos filhos e agir de acordo com a necessidade de cada fase.</li>
+    </ul>
 
-<p>Aromaterapia</p>
+    <p>&nbsp;</p>
 
-<p>Arteterapia</p>
+    <ul>
+    <li>THETAHEALING: &nbsp;Poderosa t&eacute;cnica de cura baseada em conceitos da f&iacute;sica qu&acirc;ntica, parte da ideia de que criamos nossa realidade a partir das nossas cren&ccedil;as ou mem&oacute;rias energ&eacute;ticas do nosso DNA.</li>
+    </ul>
 
-<p>Florais de Bach</p>
+    <p>&nbsp;</p>
 
-<p>Fitoterapia</p>
+    <ul>
+    <li>ESS&Ecirc;NCIA DE CRISTAIS DE OZ: S&atilde;o ess&ecirc;ncias de cristais elaboradas com a finalidade de recarregar a energia vital do corpo f&iacute;sico e emocional, possibilitando seu pleno equil&iacute;brio e funcionamento</li>
+    </ul>',
+'lista_servicos' => '"\\"Astropsicologia, Massagem Ayurv\\u00e9dica, Psicologia Transpessoal, Regress\\u00e3o, Orienta\\u00e7\\u00e3o Familiar, Orienta\\u00e7\\u00e3o \\u00e1 pais, Tarot Terap\\u00eautico, Thetahealing, Ess\\u00eancia de Cristais de Oz\\""',
+'created_at' => '2018-08-14 21:11:55',
+'updated_at' => '2018-09-01 11:10:22',
+'deleted_at' => NULL,
+            ));
 
-<p>Geoterapia</p>
+\App\Models\GrupoServico::create(array (
 
-<p>Hidroterapia</p>
+    'titulo' => 'NATUROLOGIA',
+    'descricao_grupo' => '<p>Com base nas Medicinas Tradicionais Indiana (Ayurveda), Chinesa e Ind&iacute;gena a Naturologia atua na recupera&ccedil;&atilde;o e promo&ccedil;&atilde;o da sa&uacute;de integral, tratando os sintomas e suas causas mais profundas de forma segura e eficiente por meio das terapias naturais:&nbsp;</p>
 
-<p>Massoterapia</p>
+    <p>Aromaterapia</p>
 
-<p>Reflexoterapia</p>
+    <p>Arteterapia</p>
 
-<p>Cromoterapia</p>
+    <p>Florais de Bach</p>
 
-<p>Com Jo&atilde;o Maciel (Natur&oacute;logo)</p>
+    <p>Fitoterapia</p>
 
-<p>&nbsp;</p>',
-                'descricao_servicos' => '<h2>Ayurveda</h2>
+    <p>Geoterapia</p>
+
+    <p>Hidroterapia</p>
+
+    <p>Massoterapia</p>
+
+    <p>Reflexoterapia</p>
+
+    <p>Cromoterapia</p>
+
+    <p>Com Jo&atilde;o Maciel (Natur&oacute;logo)</p>
+
+    <p>&nbsp;</p>',
+'descricao_servicos' => '<h2>Ayurveda</h2>
 
 <p>&Eacute; considerada uma das mais antigas abordagens de cuidado do mundo e significa Ci&ecirc;ncia ou Conhecimento da Vida. Nascida da observa&ccedil;&atilde;o, experi&ecirc;ncia e o uso de recursos naturais para desenvolver um sistema &uacute;nico de cuidado, este conhecimento estruturado agrega em si mesmo princ&iacute;pios relativos &agrave; sa&uacute;de do corpo f&iacute;sico, de forma a n&atilde;o desvincul&aacute;-los e considerando os campos energ&eacute;tico, mental e espiritual. A OMS descreve sucintamente o Ayurveda, reconhecendo sua utiliza&ccedil;&atilde;o para prevenir e curar doen&ccedil;as, e reconhece que esta n&atilde;o &eacute; apenas um sistema terap&ecirc;utico, mas tamb&eacute;m uma maneira de viver. No Ayurveda a investiga&ccedil;&atilde;o diagn&oacute;stica leva em considera&ccedil;&atilde;o tecidos corporais afetados, humores, local em que a doen&ccedil;a est&aacute; localizada, resist&ecirc;ncia e vitalidade, rotina di&aacute;ria, h&aacute;bitos alimentares, gravidade das condi&ccedil;&otilde;es cl&iacute;nicas, condi&ccedil;&atilde;o de digest&atilde;o, detalhes pessoais, sociais, situa&ccedil;&atilde;o econ&ocirc;mica e ambiental da pessoa. Os tratamentos no Ayurveda levam em considera&ccedil;&atilde;o a singularidade de cada pessoa, de acordo com o dosha (humores biol&oacute;gicos) do indiv&iacute;duo. Assim, cada tratamento &eacute; planejado de forma individual. S&atilde;o utilizadas t&eacute;cnicas de relaxamento, massagens, plantas medicinais, minerais, posturas corporais (&aacute;sanas), pranayamas (t&eacute;cnicas respirat&oacute;rias), mudras (posi&ccedil;&otilde;es e exerc&iacute;cios) e o cuidado diet&eacute;tico.</p>
 
@@ -248,13 +244,11 @@ Pr&aacute;tica terap&ecirc;utica que consiste na manipula&ccedil;&atilde;o (mass
 <h2>Reflexoterapia</h2>
 
 <p>Pr&aacute;tica terap&ecirc;utica que utiliza est&iacute;mulos em &aacute;reas reflexas &ndash; os microssistemas e pontos reflexos do corpo existentes nos p&eacute;s, m&atilde;os e orelhas &ndash; para auxiliar na elimina&ccedil;&atilde;o de toxinas, na seda&ccedil;&atilde;o da dor e no relaxamento. Parte do princ&iacute;pio que o corpo se encontra atravessado por meridianos que o dividem em diferentes regi&otilde;es, as quais t&ecirc;m o seu reflexo, principalmente nos p&eacute;s ou nas m&atilde;os, e permitem, quando massageados, a reativa&ccedil;&atilde;o da homeostase e do equil&iacute;brio nas regi&otilde;es com algum tipo de bloqueio. Tamb&eacute;m recebe as denomina&ccedil;&otilde;es de reflexologia ou terapia reflexa por trabalhar com os microssistemas, &aacute;reas espec&iacute;ficas do corpo (p&eacute;s, m&atilde;os, orelhas) que se conectam energeticamente e representam o organismo em sua totalidade.</p>',
-                'lista_servicos' => '"\\"Aromaterapia, Auriculoterapia, Cristalterapia, Cromopuntura, Fitoterapia, Florais de Bach, Geoterapia, Hidroterapia, Iridologia, Massoterapia, Reflexoterapia\\""',
-                'created_at' => '2018-08-14 21:11:55',
-                'updated_at' => '2018-08-29 15:30:26',
-                'deleted_at' => NULL,
-            ),
-        ));
-        
-        
+'lista_servicos' => '"\\"Aromaterapia, Auriculoterapia, Cristalterapia, Cromopuntura, Fitoterapia, Florais de Bach, Geoterapia, Hidroterapia, Iridologia, Massoterapia, Reflexoterapia\\""',
+'created_at' => '2018-08-14 21:11:55',
+'updated_at' => '2018-08-29 15:30:26',
+'deleted_at' => NULL,
+            ));
+
     }
 }
