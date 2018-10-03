f31669ddaaacc5d64a04b9e72ad9e94a0a7debbb
Thiago Braga <thibraga06@gmail.com>
2018-10-03 00:09:23 -0300

    Página "Em breve..." até o lançamento do site
    Habilitei o maintenance mode do Laravel executando
    
    ```
    php artisan down
    ```
    
    Assim, podemos customizar uma landing page básica até que o site vá ao
    ar oficialmente.

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,14 @@
+@extends('errors::layout')
+
+@section('title', 'Instituto Shanti | Em breve...')
+
+@section('message')
+  <img src="images/logo-shanti.png" width="250" height="250" /><br/>
+  <br/>
+  Em breve...<br/>
+  <br/>
+  <a href="https://grupotesseract.com.br" target="_blank" rel="noopener noreferrer" style="text-decoration: none">
+    <small style="font-size: 12px; font-weight: 600; color: #aaa">Desenvolvido por</small><br/>
+    <img src="https://res.cloudinary.com/tesseract/image/upload/c_scale,w_50/v1531876112/abenepi2/logo-tesseract-preto.png" />
+  </a>
+@stop
